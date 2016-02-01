f467b5ed141dfa0e8443cc6a58c17a439e84f5f8
ragez <ragez@localhost>
2016-02-01 09:57:43 +0100

    test2

--- Projet Vortex/src/Projet/Test.java
+++ Projet Vortex/src/Projet/Test.java
@@ -4,7 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Searcher searcher = new IndexSearcher("./webindex/");
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		Query query = QueryParser.parse(searchQuery, "contentKeywords", analyzer); 
+		Hits hits = searcher.search(query);
 
 	}
+	
+	
 
 }
